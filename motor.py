f7c60afa6cb4f303c9c73f51cd2fd29d89c4d2be
Baicen Li <lbc2017015445@gmail.com>
2023-03-04 15:52:29 -0600

    Starting work on refactoring.

---
+++ motor.py
@@ -0,0 +1,31 @@
+import constants as c
+import numpy as np
+import pyrosim.pyrosim as pyrosim
+import pybullet as p
+class MOTOR:
+    def __init__(self,robot,jointName):
+        self.robot=robot
+        self.jointName=jointName
+        #self.robotId=robotId
+        #self.robotId=p.loadURDF("body.urdf")
+        self.values=np.zeros(1000)
+    def Set_Value(self,t,robotId):
+        self.robotId=robotId
+        self.amplitude=c.amplitude
+        self.frequency=c.frequency
+        self.phaseOffset=c.phaseOffset
+        if self.jointName == "Torso_BackLeg":
+           self.frequency=self.frequency/2
+        else:
+            self.frequency=self.frequency
+        pyrosim.Set_Motor_For_Joint(
+            bodyIndex =self.robotId,
+            jointName = self.jointName,
+            controlMode = p.POSITION_CONTROL,
+            targetPosition = self.amplitude * np.sin(self.frequency* t*np.pi*2/100+ self.phaseOffset),
+            maxForce = 23)
+        self.values[t]=np.sin(self.amplitude * np.sin(self.frequency* t*np.pi*2/100+ self.phaseOffset))
+        #if t==1000-1:
+         #   print(self.jointName,self.values[t])
+        return self.values[t]
+    
