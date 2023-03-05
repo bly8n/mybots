c3d798e942028bfae8971443c40424b2efdfbfe2
Baicen Li <lbc2017015445@gmail.com>
2023-03-04 23:11:38 -0600

    starting work on neurons

--- motor.py
+++ motor.py
@@ -9,7 +9,7 @@ class MOTOR:
         #self.robotId=robotId
         #self.robotId=p.loadURDF("body.urdf")
         self.values=np.zeros(1000)
-    def Set_Value(self,t,robotId):
+    def Set_Value(self,desiredAngle,robotId):
         self.robotId=robotId
         self.amplitude=c.amplitude
         self.frequency=c.frequency
@@ -22,10 +22,11 @@ class MOTOR:
             bodyIndex =self.robotId,
             jointName = self.jointName,
             controlMode = p.POSITION_CONTROL,
-            targetPosition = self.amplitude * np.sin(self.frequency* t*np.pi*2/100+ self.phaseOffset),
+            targetPosition = self.amplitude * np.sin(self.frequency*desiredAngle*np.pi*2/100+ self.phaseOffset),
             maxForce = 23)
-        self.values[t]=np.sin(self.amplitude * np.sin(self.frequency* t*np.pi*2/100+ self.phaseOffset))
+        self.values[desiredAngle]=np.sin(self.amplitude * np.sin(self.frequency*desiredAngle*np.pi*2/100+ self.phaseOffset))
         #if t==1000-1:
          #   print(self.jointName,self.values[t])
-        return self.values[t]
+        return self.values[desiredAngle]
+    #
     
