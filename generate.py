89f2e5edb0564645dc79384c86c3ff21db8574fb
bly8n <114317667+bly8n@users.noreply.github.com>
2023-03-19 20:33:49 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -32,5 +32,8 @@ def Generate_Brain(x,y,z):
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1 )
+    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1 )
+    
     pyrosim.End()
 Generate_Brain(x,y,z)
