49abeab2e4cc81638cfa71102903c4aa98749da5
Baicen Li <lbc2017015445@gmail.com>
2023-03-21 09:25:40 -0500

    starting work on parallelHC

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
 import os
 import time
-from hillclimber import HILL_CLIMBER
-hc = HILL_CLIMBER()
-hc.Evolve('DIRECT')
-hc.Show_Best()
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
+
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve('GUI')
+phc.Show_Best()
 #for i in range(5):
     #os.system("python generate.py")
     #time.sleep(2)
